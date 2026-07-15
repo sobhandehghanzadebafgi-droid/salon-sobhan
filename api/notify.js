a2645257c6c02f18133605dc2371a5fb6fff9279
sobhandehghanzadebafgi-droid <sobhandehghanzadebafgi@gmail.com>
2026-07-15 14:26:24 +0330

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -8,12 +8,12 @@ export default async function handler(req, res) {
     } else {
       dateLabel = 'تست'; time = '00:00'; name = 'تست دستی'; phone = '0000000000';
     }
-    var msg = 'نوبت جدید ثبت شد\n' + dateLabel + ' ساعت ' + time + '\nمشتری: ' + name + '\nتماس: ' + phone;
+    var msg = 'نوبت جدید ثبت شد\n' + dateLabel + ' ساعت ' + time + '\nمشتری: ' + name + '\nتماس: ' + phone + '\nلغو 11';
 
     var r = await fetch('https://console.melipayamak.com/api/send/simple/3967054f-57b3-4d26-b916-a20b50e34706', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ from: '1000029154759', to: '09382838181', text: msg })
+      body: JSON.stringify({ from: '10000291547590', to: '09382838181', text: msg })
     });
     var data = await r.text();
     res.status(200).json({ ok: true, status: r.status, data: data });
