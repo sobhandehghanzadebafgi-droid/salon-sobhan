c9b3334e6e2d453db012b9adc7195e752f0e8d64
sobhandehghanzadebafgi-droid <sobhandehghanzadebafgi@gmail.com>
2026-07-15 14:20:02 +0330

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -1,22 +1,22 @@
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
   try {
-    var body = req.body;
-    if (typeof body === 'string') body = JSON.parse(body);
-    var dateLabel = body.dateLabel, time = body.time, name = body.name, phone = body.phone;
+    var dateLabel, time, name, phone;
+    if (req.method === 'POST') {
+      var body = req.body;
+      if (typeof body === 'string') body = JSON.parse(body);
+      dateLabel = body.dateLabel; time = body.time; name = body.name; phone = body.phone;
+    } else {
+      dateLabel = 'تست'; time = '00:00'; name = 'تست دستی'; phone = '0000000000';
+    }
     var msg = 'نوبت جدید ثبت شد\n' + dateLabel + ' ساعت ' + time + '\nمشتری: ' + name + '\nتماس: ' + phone;
 
-    var r = await fetch('https://console.melipayamak.com/api/send/simple/2d53c1c0-69be-4fde-9846-f98ff7f626f0', {
+    var r = await fetch('https://console.melipayamak.com/api/send/simple/3967054f-57b3-4d26-b916-a20b50e34706', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ from: '10000291547590', to: '09382838181', text: msg })
+      body: JSON.stringify({ from: '1000029154759', to: '09382838181', text: msg })
     });
     var data = await r.text();
-    console.log('MELIPAYAMAK_RESPONSE_STATUS:', r.status);
-    console.log('MELIPAYAMAK_RESPONSE_BODY:', data);
-    res.status(200).json({ ok: true, data: data });
+    res.status(200).json({ ok: true, status: r.status, data: data });
   } catch (e) {
     res.status(500).json({ ok: false, error: String(e) });
   }
